53ee36d2cd4e02eadbed2b9db7246635712e1c96
celsodasecond <75917932+celsodasecond@users.noreply.github.com>
2022-11-24 17:54:22 +0800

    Initial Code

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function getButtonText() {
+    return 'Hi There!';
+}
+
+// Function Based Component using ES 2015 Syntax
+const App = () => {
+    return(
+        <div>
+            <label htmlFor='name' className='label'>enter e-mail</label>
+            <input id="name" type="text"></input>
+            <button style={{backgroundColor: 'red', color: 'white'}}>
+                {getButtonText()}
+            </button>
+        </div>
+    )
+}
+
+// To show to the user
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
